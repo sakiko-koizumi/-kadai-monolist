8ae019200c72220fc45f2599b06eb793a41de3d3
sakiko.koizumi <sakiko.koizumi@rakuten.com>
2018-06-12 06:35:14 +0000

    user registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,14 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+    
     public function boot()
     {
-        //
+         \Schema::defaultStringLength(191);
     }
 
     /**
